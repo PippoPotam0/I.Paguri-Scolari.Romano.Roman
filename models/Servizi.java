f6b46004982c4e1077a3098a7e629cf2490fd3c9
PippoPotamo <fscolari@cerebotani.it>
2023-12-07 11:25:59 +0100

    add Servizi

--- models/Servizi.java
+++ models/Servizi.java
@@ -1,5 +1,10 @@
 package models;
 
-public class Servizi {
-    
+public class Servizi extends Prodotto {
+    public String tempoPrestazione;
+
+    public Servizi(String nome, int quantita, double prezzo, String tempoPrestazione) {
+        super(nome, quantita, prezzo);
+        this.tempoPrestazione = tempoPrestazione;
+    }
 }
