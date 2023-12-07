7456527a45b73077956d852bdcb637dca665a690
gnomo_dave <dromano@cerebotani.it>
2023-12-07 11:22:49 +0100

    Update Prodotto.java

--- Prodotto.java
+++ Prodotto.java
@@ -1,19 +1,44 @@
 
 
 
-public class Prodotto{
+public abstract class Prodotto{
 
     private String nome;
     private int quantita;
     private double prezzo; 
 
     public Prodotto(String nome, int quantita, double prezzo){
-        nome = this.nome;
-        quantita = this.quantita;
+        this.nome = nome;
+        this.quantita = quantita;
         prezzo = this.prezzo;
     }
 
-    
+    public String toString(){
+        return "Nome: " + nome + "Quantità: " + quantita + "Prezzo: " + prezzo;
+    }
 
+    public String getNome(){  
+        return  nome; 
+    } 
+
+    public int getQuantita(){
+        return quantita;
+    }   
+
+    public double getPrezzo(){
+        return prezzo;
+    }       
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }       
+
+    public void setQuantita(int quantita){
+        this.quantita = quantita;
+    }       
+
+    public void setPrezzo(double prezzo){
+        this.prezzo = prezzo;
+    }      
 
 }
